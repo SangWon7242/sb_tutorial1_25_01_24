9166aed3633f00cb4ba5675c4e73ed921126eeb4
SangWon7242 <gotkdqja@gmail.com>
2025-02-04 21:38:10 +0900

    Member, MemberRepository 도입

--- src/main/java/com/sbs/tutorial1/bouendedContext/member/service/MemberService.java
+++ src/main/java/com/sbs/tutorial1/bouendedContext/member/service/MemberService.java
@@ -1,17 +1,28 @@
 package com.sbs.tutorial1.bouendedContext.member.service;
 
 import com.sbs.tutorial1.bouendedContext.base.rsData.RsData;
+import com.sbs.tutorial1.bouendedContext.member.controller.MemberController;
+import com.sbs.tutorial1.bouendedContext.member.entity.Member;
+import com.sbs.tutorial1.bouendedContext.member.repository.MemberRepository;
 import org.springframework.stereotype.Service;
 
 @Service // 스프링부트가 해당 클래스를 서비스로 인식
 public class MemberService {
+  private MemberRepository memberRepository;
+
+  public MemberService() {
+    memberRepository = new MemberRepository();
+  }
+
   public RsData tryLogin(String username, String password) {
-    if(!password.equals("1234")) {
-      return RsData.of("F-4", "비밀번호가 일치하지 않습니다.");
+    Member member = memberRepository.findByUsername(username);
+
+    if(member == null) {
+      return RsData.of("F-3", "%s(은)는 존재하지 않는 회원입니다.".formatted(username));
     }
 
-    if(!username.equals("user1")) {
-      return RsData.of("F-3", "%s(은)는 존재하지 않는 회원입니다.".formatted(username));
+    if(!member.getPassword().equals(password)) {
+      return RsData.of("F-4", "비밀번호가 일치하지 않습니다.");
     }
 
     return RsData.of("S-1", "%s님 환영합니다.".formatted(username));
