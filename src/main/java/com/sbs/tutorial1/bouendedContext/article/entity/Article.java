cfbdae267898d59cb65e315b212af30a566e5a07
SangWon7242 <gotkdqja@gmail.com>
2025-02-07 21:09:29 +0900

    Article에 auto_increment 적용, 생성, 수정날짜 반영

--- src/main/java/com/sbs/tutorial1/bouendedContext/article/entity/Article.java
+++ src/main/java/com/sbs/tutorial1/bouendedContext/article/entity/Article.java
@@ -1,10 +1,15 @@
 package com.sbs.tutorial1.bouendedContext.article.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
 
 @AllArgsConstructor
 @Getter
@@ -12,7 +17,10 @@ import lombok.Setter;
 @Entity
 public class Article {
   @Id // primary key
+  @GeneratedValue(strategy = IDENTITY) // AUTO_INCREMENT
   private long id;
+  private LocalDateTime createDate; // 데이터 생성날짜
+  private LocalDateTime modifyDate; // 데이터 수정날짜
   private String subject;
   private String content;
 }
