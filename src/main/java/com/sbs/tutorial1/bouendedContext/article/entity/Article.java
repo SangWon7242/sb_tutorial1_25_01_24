3c9e538a9810d4315f136f43c16ffccb53440c49
SangWon7242 <gotkdqja@gmail.com>
2025-02-07 21:33:47 +0900

    등록날짜, 수정날짜 자동 갱신

--- src/main/java/com/sbs/tutorial1/bouendedContext/article/entity/Article.java
+++ src/main/java/com/sbs/tutorial1/bouendedContext/article/entity/Article.java
@@ -1,27 +1,33 @@
 package com.sbs.tutorial1.bouendedContext.article.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 @Entity
 @Builder
+@EntityListeners(AuditingEntityListener.class) // Auditing 활성화
+// AuditingEntityListener : INSERT, UPDATE 감지하여 필드 값을 재설정
 public class Article {
   @Id // primary key
   @GeneratedValue(strategy = IDENTITY) // AUTO_INCREMENT
   private long id;
+  
+  @CreatedDate
+  @Column(updatable = false) // 얻데이트 시 수정되지 않도록 설정
   private LocalDateTime createDate; // 데이터 생성날짜
+  
+  @LastModifiedDate // 마지막 수정 시점 자동 갱신
   private LocalDateTime modifyDate; // 데이터 수정날짜
   private String subject;
   private String content;
