2b2fd4ca2de9de910d8151ee1ed041454c85516c
SangWon7242 <gotkdqja@gmail.com>
2025-02-07 21:59:18 +0900

    테스트 회원 추가

--- db/db.sql
+++ db/db.sql
@@ -2,3 +2,10 @@
 DROP DATABASE IF EXISTS tutorial1__dev;
 CREATE DATABASE tutorial1__dev;
 USE tutorial1__dev;
+
+# 테스트 회원 생성
+INSERT INTO `member`
+SET create_date = NOW(),
+modify_date = NOW(),
+username = 'user1',
+`password` = '1234';
