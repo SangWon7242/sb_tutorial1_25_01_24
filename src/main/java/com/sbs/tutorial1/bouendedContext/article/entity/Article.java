b5fe94f5cbc312b39753d305120839be508cec9b
SangWon7242 <gotkdqja@gmail.com>
2025-02-11 19:48:06 +0900

    spring.jpa.hibernate.ddl-auto를 update로 변경

--- src/main/java/com/sbs/tutorial1/bouendedContext/article/entity/Article.java
+++ src/main/java/com/sbs/tutorial1/bouendedContext/article/entity/Article.java
@@ -24,11 +24,12 @@ public class Article {
   private long id;
   
   @CreatedDate
-  @Column(updatable = false) // 얻데이트 시 수정되지 않도록 설정
+  @Column(updatable = false) // 업데이트 시 수정되지 않도록 설정
   private LocalDateTime createDate; // 데이터 생성날짜
   
   @LastModifiedDate // 마지막 수정 시점 자동 갱신
   private LocalDateTime modifyDate; // 데이터 수정날짜
+
   private String subject;
   private String content;
 }
