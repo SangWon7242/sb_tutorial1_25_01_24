88d73e76cb04bba107354dc17b5c87f35441fff7
SangWon7242 <gotkdqja@gmail.com>
2025-02-11 21:15:19 +0900

    회원 가입 후 root 페이지로 리다이렉트

--- src/main/java/com/sbs/tutorial1/bouendedContext/member/controller/MemberController.java
+++ src/main/java/com/sbs/tutorial1/bouendedContext/member/controller/MemberController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/member")
@@ -22,25 +23,17 @@ public class MemberController {
   }
 
   @PostMapping("/join")
-  @ResponseBody
-  public RsData join(String username, String password) {
-    if (username == null || username.trim().isEmpty()) {
-      return RsData.of("F-1", "로그인 아이디를 입력해주세요.");
-    }
-
-    if (password == null || password.trim().isEmpty()) {
-      return RsData.of("F-2", "로그인 비번을 입력해주세요.");
-    }
-
+  public String join(String username, String password, RedirectAttributes redirectAttributes) {
     Member member = memberService.findByUsername(username);
 
     if(member != null) {
-      return RsData.of("F-3", "이미 가입 된 회원입니다.");
+      redirectAttributes.addFlashAttribute("rsData", RsData.of("F-3", "이미 가입 된 회원입니다."));
+      return "redirect:/";
     }
 
-    RsData rsData = memberService.join(username, password);
+    memberService.join(username, password);
 
-    return rsData;
+    return "redirect:/";
   }
 
   @GetMapping("/login")
